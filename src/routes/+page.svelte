c28ff8e89946ba76a361d311901aa05e481b59c0
Janine <132993862+alphablondie111@users.noreply.github.com>
2023-05-28 19:17:04 +0200

    all questions added

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,7 +11,15 @@
 		nature: 1,
 		museums: 1, 
 		food: 1,
-		budget: 1
+		budget: 1,
+		architecture: 1,
+		party: 1,
+		safety: 1,
+		inclusive: 1,
+		walking: 1,
+		beach: 1,
+		adventurous: 1,
+		luxuriously: 1
 	};
 
 	interface Place {
@@ -61,60 +69,109 @@
 			score={answers.beaches}
 		/>
 		<Questions
-			question="How much do you like warm weather?"
+			question="1 How much do you like warm weather?"
 			on:change={(event) => {
 				answers = { ...answers, warmWeather: event.detail };
 			}}
 			score={answers.warmWeather}
 		/>
 		<Questions
-			question="How much do you enjoy rain?"
+			question="2 How much do you enjoy rain?"
 			on:change={(event) => {
 				answers = { ...answers, rain: event.detail };
 			}}
 			score={answers.rain}
 		/>
 		<Questions
-			question="How much do you enjoy going to the casino?"
+			question="3 How much do you enjoy going to the casino?"
 			on:change={(event) => {
 				answers = { ...answers, casino: event.detail };
 			}}
 			score={answers.casino}
 		/>
 		<Questions
-			question="How important is exploring nature to you?"
+			question="4 How important is exploring nature to you?"
 			on:change={(event) => {
 				answers = { ...answers, nature: event.detail };
 			}}
 			score={answers.nature}
 		/>
 		<Questions
-			question="Do you enjoy spending time in museums?"
+			question="5 Do you enjoy spending time in museums?"
 			on:change={(event) => {
 				answers = { ...answers, museums: event.detail };
 			}}
 			score={answers.museums}
 		/>
 		<Questions
-			question="Are you foodie?"
+			question="6 Are you foodie?"
 			on:change={(event) => {
 				answers = { ...answers, food: event.detail };
 			}}
 			score={answers.food}
 		/>
 		<Questions
-			question="How much are you willing to spend on your vacation?"
+			question="7 How much are you willing to spend on your vacation?"
 			on:change={(event) => {
 				answers = { ...answers, budget: event.detail };
 			}}
 			score={answers.budget}
 		/>
 		<Questions
-			question="Do you like architecture?"
+			question="8 Do you like architecture?"
 			on:change={(event) => {
 				answers = { ...answers, architecture: event.detail };
 			}}
 			score={answers.architecture}
+		/>
+		<Questions
+			question="9 Do you wanto to party on your vacation?"
+			on:change={(event) => {
+				answers = { ...answers, party: event.detail };
+			}}
+			score={answers.party}
+		/>
+		<Questions
+			question="10 How important is safety while vacationing to you?"
+			on:change={(event) => {
+				answers = { ...answers, safety: event.detail };
+			}}
+			score={answers.safety}
+		/>
+		<Questions
+			question="11 Do you like all inclusive holidays?"
+			on:change={(event) => {
+				answers = { ...answers, inclusive: event.detail };
+			}}
+			score={answers.inclusive}
+		/>
+		<Questions
+			question="12 Do you mind walking a lot?"
+			on:change={(event) => {
+				answers = { ...answers, walking: event.detail };
+			}}
+			score={answers.walking}
+		/>
+		<Questions
+			question="13 How bad do you want to spend time at a beach?"
+			on:change={(event) => {
+				answers = { ...answers, beach: event.detail };
+			}}
+			score={answers.beach}
+		/>
+		<Questions
+			question="14 How adventurous are you?"
+			on:change={(event) => {
+				answers = { ...answers, adventurous: event.detail };
+			}}
+			score={answers.adventurous}
+		/>
+		<Questions
+			question="15 Do you like to travel luxuriously?"
+			on:change={(event) => {
+				answers = { ...answers, luxuriously: event.detail };
+			}}
+			score={answers.luxuriously}
 		/>
 		<Button color="purple" on:click={submit}>Submit</Button>
 	</div>
